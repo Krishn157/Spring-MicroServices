1be2c9571e3cf3b64872c60622e5a9c1645b4d3c
krishn157 <krishn.kumar2018@vitstudent.ac.in>
2021-03-17 23:13:54 +0530

    swagger-documentation

--- src/main/java/com/krishn/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/krishn/rest/webservices/restfulwebservices/user/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "All details about the user")
 public class User {
     
     private Integer id;
 
     @Size(min=2, message = "Name should have atleast 2 characters")
+    @ApiModelProperty(notes ="Name should have atleast 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birthday should be a past date")
     private Date birthDate;
 
 
